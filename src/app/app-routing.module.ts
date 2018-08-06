779bda9f88f1f8f2a79a04e1cf9dba28e053bf1a
Juan David Garzon Alzate <juan.alzate@yuxiglobal.com>
2018-08-06 14:09:13 -0500

    lazy loading and order in the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { MainPanelComponent } from './dashboard/main-panel/main-panel.component';
-import { SingUpComponent } from './login/sing-up/sing-up.component';
-import { SingInComponent } from './login/sing-in/sing-in.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: MainPanelComponent },
-  { path: 'home', component: MainPanelComponent },
-  { path: 'login', component:  SingInComponent},
-  { path: 'register', component:  SingUpComponent}
+  { path: '', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  { path: 'home', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  { path: 'login', loadChildren:  './login/login.module#LoginModule'},
+  { path: 'registry', loadChildren: './sing-up/sing-up.module#SingUpModule' },
+  { path: 'main', loadChildren:  './main-cart/main-cart.module#MainCartModule'}
 ];
 
 @NgModule({
